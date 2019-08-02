da58e235b644a74dedf5d2b2634c0c238ee45ffd
Codee King <codee.king@gmail.com>
2019-08-02 13:57:43 -0500

    update client name

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-highlights/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Anubhav',
-  subHeading: 'Web Developer',
+  heading: 'Branches of Life Healing Center',
+  subHeading: 'Mental Health Therapists',
   // social
   socialLinks: [
     {
